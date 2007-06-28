a4501d5652b709895bcf79e42b70f14a86744766
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2007-06-28 11:32:26 +0000

    The Data base class now include a vfields() method, that  lists 'volatile' fields, data stored inside the object  (and accessible via get() and set()), but that is not  considered as stored on disk. This is used by the renderers  to store the abstract and body of stories.

--- Webon2/Data/Story.pm
+++ Webon2/Data/Story.pm
@@ -3,5 +3,6 @@ package Webon2::Data::Story;
 use base 'Webon2::Data::BASE';
 
 sub fields { return qw(id topic_id title date userid format hits ctime content); }
+sub vfields { return qw(abstract body); }
 
 1;
