07cd3afd2435a8382652f79cdb93fa57b282a9fd
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2007-06-29 16:18:43 +0000

    Deleted all ::Data::*.pm source files (they are all defined in Data.pm).

--- Webon2/Data/Topic.pm
+++
@@ -1,7 +0,0 @@
-package Webon2::Data::Topic;
-
-use base 'Webon2::Data::BASE';
-
-sub fields { return qw(id name editors max_stories internal); }
-
-1;
--- Webon2/Data/User.pm
+++
@@ -1,7 +0,0 @@
-package Webon2::Data::User;
-
-use base 'Webon2::Data::BASE';
-
-sub fields { return qw(id username email password can_upload is_admin); }
-
-1;
