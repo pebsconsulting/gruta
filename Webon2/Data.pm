cfb8d2c42e7ebc3f9f54026d25e8c30f1233a35d
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2007-06-29 16:18:33 +0000

    Added all ::Data:: subclasses from their own source file  to Data.pm.

--- Webon2/Data.pm
+++ Webon2/Data.pm
@@ -1,5 +1,67 @@
 package Webon2::Data;
 
+package Webon2::Data::BASE;
+
+sub fields { return (); }
+sub vfields { return (); }
+
+sub new {
+	my $class	= shift;
+	my %args	= @_;
+
+	my $self = bless({ }, $class);
+
+	foreach my $k ($self->fields(), $self->vfields()) {
+		$self->{$k} = $args{$k};
+	}
+
+	return $self;
+}
+
+sub get {
+	my $self	= shift;
+	my $field	= shift;
+
+	die 'get ' . ref($self) . " field '$field'?" unless exists $self->{$field};
+
+	return $self->{$field};
+}
+
+sub set {
+	my $self	= shift;
+	my $field	= shift;
+
+	die 'set ' . ref($self) . " field '$field'?" unless exists $self->{$field};
+
+	$self->{$field} = shift;
+
+	return $self->{$field};
+}
+
+
+package Webon2::Data::Topic;
+
+use base 'Webon2::Data::BASE';
+
+sub fields { return qw(id name editors max_stories internal); }
+
+package Webon2::Data::Story;
+
+use base 'Webon2::Data::BASE';
+
+sub fields { return qw(id topic_id title date userid format hits ctime content); }
+sub vfields { return qw(abstract body); }
+
+package Webon2::Data::User;
+
+use base 'Webon2::Data::BASE';
+
+sub fields { return qw(id username email password can_upload is_admin); }
+
+##################################################
+
+package Webon2::Data;
+
 sub sources { return @{$_[0]->{sources}}; }
 sub template { return $_[0]->{template}; }
 
