032d4167421b5ad99ec097979ea148db861c8e04
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2007-07-03 06:20:56 +0000

    r53254@cracovia:  angel | 2007-07-03 08:24:57 +0200  Fixed a bug when reading the password field in login().
    git-svn-id: svn+ssh://svn.triptico.com/home/angel/svn/gruta/trunk@6400 c87de0a0-a11c-0410-a1e5-866214bc28b2

--- g.pl
+++ g.pl
@@ -35,7 +35,7 @@ my $w = Gruta::Data->new(
 #	base		=>	$base,
 #	upload		=>	[ "${base}/img" ],
 #	templates	=>	[ "${base}/templates" ],
-	sources		=>	[ $src2 ],
+	sources		=>	[ $src ],
 	renderers	=>	[ $rndr, $rndr2, $rndr3 ],
 	template	=>	$tmpl
 );
