e9eae0df447829d6ad7e88850009010aba99078a
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2007-07-06 11:57:13 +0000

    r53447@cracovia:  angel | 2007-07-06 13:57:00 +0200  Test code change to test changes in Artemus.
    git-svn-id: svn+ssh://svn.triptico.com/home/angel/svn/gruta/trunk@6452 c87de0a0-a11c-0410-a1e5-866214bc28b2

--- g.pl
+++ g.pl
@@ -46,7 +46,7 @@ my $w = Gruta->new(
 #	templates	=>	[ "${base}/templates" ],
 	sources		=>	[ $src, $src3 ],
 	renderers	=>	[ $rndr, $rndr2, $rndr3 ],
-	template	=>	$tmpl2
+	template	=>	$tmpl
 );
 
 $w->login('angel', 'test');
@@ -54,12 +54,12 @@ $w->logout();
 
 $w->template->cgi_vars( { t => 'ADMIN', topic => 'alimentos' } );
 
-#my $str = $w->template("{-story_part|alimentos|200609200001|content}");
+my $str = $w->template->process("{-story_part|alimentos|200609200001|content}");
 #my $str = $w->template->process("{-loop_topics|_topics_as_option|\n}");
 #$str = $w->template->process("{-story_loop_by_date|noticias|10|0|_story_link_as_item_with_hits|\n}");
 #$str = $w->template->process("{-loop_renderers||\n}");
 
-my $str = $w->template->process('ADMIN');
+#my $str = $w->template->process('ADMIN');
 
 my @ts = $w->topics();
 
