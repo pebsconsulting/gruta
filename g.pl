ff9ebc1ce60942b975aba348e4906613f0f49bb2
angel <angel@c87de0a0-a11c-0410-a1e5-866214bc28b2>
2007-07-06 11:42:31 +0000

    Added test code for cgi_vars in the Template Toolkit driver.

--- g.pl
+++ g.pl
@@ -52,6 +52,8 @@ my $w = Gruta->new(
 $w->login('angel', 'test');
 $w->logout();
 
+$w->template->cgi_vars( { t => 'ADMIN', topic => 'alimentos' } );
+
 #my $str = $w->template("{-story_part|alimentos|200609200001|content}");
 #my $str = $w->template->process("{-loop_topics|_topics_as_option|\n}");
 #$str = $w->template->process("{-story_loop_by_date|noticias|10|0|_story_link_as_item_with_hits|\n}");
