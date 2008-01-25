753ffa0c98d6ea636e44495ad78097bcd701a7f2
Angel Ortega <angel@belial.triptico.com>
2008-01-25 14:14:02 +0100

    The Artemus function 'date' now accepts an optional date as argument.

--- Gruta/Template/Artemus.pm
+++ Gruta/Template/Artemus.pm
@@ -45,7 +45,13 @@ sub _artemus {
 		$f{'lt'} = sub { $_[0] < $_[1]; };
 		$f{'eq'} = sub { $_[0] eq $_[1] ? 1 : 0; };
 
-		$f{date} = sub { Gruta::Data::today($_[0]); };
+		$f{date} = sub {
+			my $fmt	= shift;
+			my $d	= shift || Gruta::Data::today();
+
+			return Gruta::Data::format_date($d, $fmt);
+		};
+
 		$f{random} = sub { $_[rand(scalar(@_))]; };
 
 		foreach my $p (Gruta::Data::Topic->new->afields()) {
