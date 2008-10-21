468f46e0a771663cb54436605308c2f57aea7fde
Angel Ortega <angel@triptico.com>
2008-10-21 13:51:54 +0200

    The driver accesor is really called 'source' (brrr.).

--- Gruta/Source/FS.pm
+++ Gruta/Source/FS.pm
@@ -131,7 +131,7 @@ sub save {
 sub touch {
 	my $self = shift;
 
-	if (! $self->driver->{dummy_touch}) {
+	if (! $self->source->{dummy_touch}) {
 		my $hits = $self->get('hits') + 1;
 
 		$self->set('hits', $hits);
