9a7e82328bccfd9d1c101055d1e722f6dae6b96f
Angel Ortega <angel@triptico.com>
2010-06-14 15:56:18 +0200

    New source method related_stories(), that returns stories related to another.

--- Gruta/Source/BASE.pm
+++ Gruta/Source/BASE.pm
@@ -44,4 +44,32 @@ sub cache_story {
 }
 
 
+sub related_stories {
+	my $self	= shift;
+	my $story	= shift;
+	my $max		= shift || 5;
+
+	my @ret = ();
+
+	# get tags
+	my @tags = $story->tags();
+
+	foreach my $i ($self->stories_by_tag(undef, join(",", @tags))) {
+		# if it's the same story, ignore
+		if ($i->[1] eq $story->get('id') &&
+					$i->[0] eq $story->get('topic_id')) {
+			next;
+		}
+
+		push(@ret, $i);
+
+		# enough of them? done
+		if (scalar(@ret) >= $max) {
+			last;
+		}
+	}
+
+	return sort { $a->[2] cmp $b->[2] } @ret;
+}
+
 1;
