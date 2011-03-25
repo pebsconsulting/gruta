daafd519ef13b8bcf4d8e6c39dc100c52bae1963
Angel Ortega <angel@triptico.com>
2011-03-25 10:39:33 +0100

    Start of new version.

--- Gruta.pm
+++ Gruta.pm
@@ -7,8 +7,8 @@ use locale;
 
 use Gruta::Data;
 
-$Gruta::VERSION				= '2.3.1';
-$Gruta::VERSION_CODENAME	= '"Sienna"';
+$Gruta::VERSION				= '2.3.2-dev';
+$Gruta::VERSION_CODENAME	= '"Arezzo"';
 
 sub source {
 	$_[0]->{source};
