ecc5822191dd0c18695bf789518344784c77b641
Angel Ortega <angel@triptico.com>
2011-09-06 12:26:45 +0200

    Fix DBI comment expiration.

--- Gruta/Source/DBI.pm
+++ Gruta/Source/DBI.pm
@@ -377,8 +377,8 @@ sub story_comments {
     }
 
 	# delete old non-approved comments for this story
-	my $sth = $self->_prepare('DELETE FROM comments WHERE ctime < ? AND ' .
-							'topic_id = ? AND story_id = ?');
+	my $sth = $self->_prepare('DELETE FROM comments WHERE approved = 0 AND ' .
+                            'ctime < ? AND topic_id = ? AND story_id = ?');
 	$self->_execute($sth, time() - (60 * 60 * 24 * $expire_days),
 				$story->get('topic_id'), $story->get('id'));
 
