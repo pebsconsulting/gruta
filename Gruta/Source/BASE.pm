9db6974914bab1771bdf4351538de4b2c7c01df0
Angel Ortega <angel@triptico.com>
2013-11-15 20:15:03 +0100

    Minor code cleaning.

--- Gruta/Source/BASE.pm
+++ Gruta/Source/BASE.pm
@@ -56,61 +56,57 @@ sub cache_story {
 
 
 sub related_stories {
-	my $self	= shift;
-	my $story	= shift;
-	my $max		= shift || 5;
+    my $self    = shift;
+    my $story   = shift;
+    my $max     = shift || 5;
 
-	my @ret = ();
+    my @ret = ();
 
-	# get tags
-	my @tags = $story->tags();
+    # get tags
+    my @tags = $story->tags();
 
-	my $id			= $story->get('id');
-	my $topic_id	= $story->get('topic_id');
+    my $id          = $story->get('id');
+    my $topic_id    = $story->get('topic_id');
 
-	if (scalar(@tags)) {
-		my %h = ();
-		my @ret1 = ();
-		my @ret2 = ();
+    if (scalar(@tags)) {
+        my %h = ();
+        my @ret1 = ();
+        my @ret2 = ();
 
-		# mark the current story as already used
-		$h{$topic_id . '/' . $id} = 1;
+        # mark the current story as already used
+        $h{$topic_id . '/' . $id} = 1;
 
-		foreach my $i ($self->stories_by_tag(undef, join(",", @tags))) {
-			my $k = $i->[0] . '/' . $i->[1];
+        foreach my $i ($self->stories_by_tag(undef, join(",", @tags))) {
+            my $k = $i->[0] . '/' . $i->[1];
 
-			if (exists($h{$k})) {
-				next;
-			}
+            if (!exists($h{$k})) {
+                push(@ret1, $i);
+                $h{$k}++;
+            }
+        }
 
-			push(@ret1, $i);
-			$h{$k}++;
-		}
+        @ret1 = sort { $b->[2] cmp $a->[2] } @ret1;
 
-		@ret1 = sort { $b->[2] cmp $a->[2] } @ret1;
+        # if not enough, get others, tag by tag
+        if (scalar(@ret1) < $max) {
+            while (scalar(@tags)) {
+                foreach my $i ($self->stories_by_tag(undef, shift(@tags))) {
+                    my $k = $i->[0] . '/' . $i->[1];
 
-		# if not enough, get others, tag by tag
-		if (scalar(@ret1) < $max) {
-			while (scalar(@tags)) {
-				foreach my $i ($self->stories_by_tag(undef, shift(@tags))) {
-					my $k = $i->[0] . '/' . $i->[1];
+                    if (!exists($h{$k})) {
+                        push(@ret2, $i);
+                        $h{$k}++;
+                    }
+                }
+            }
 
-					if (exists($h{$k})) {
-						next;
-					}
+            @ret2 = sort { $b->[2] cmp $a->[2] } @ret2;
+        }
 
-					push(@ret2, $i);
-					$h{$k}++;
-				}
-			}
-		}
+        @ret = (@ret1, @ret2);
+    }
 
-		@ret2 = sort { $b->[2] cmp $a->[2] } @ret2;
-
-		@ret = (@ret1, @ret2);
-	}
-
-	return scalar(@ret) > $max ? @ret[0 .. ($max - 1)] : @ret;
+    return scalar(@ret) > $max ? @ret[0 .. ($max - 1)] : @ret;
 }
 
 1;
