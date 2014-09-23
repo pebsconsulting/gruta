38bef8aa1dcd79831efa54866f5e677e5e0cabb0
Angel Ortega <angel@triptico.com>
2014-09-23 18:36:51 +0200

    New file Gruta/FCGI.pm.

---
+++ Gruta/FCGI.pm
@@ -0,0 +1,14 @@
+package Gruta::FCGI;
+
+use base 'Gruta::CGI';
+
+use CGI::Fast qw(:standard);
+use Carp;
+
+sub run {
+    my $self = shift;
+
+    while ($self->{cgi} = new CGI::Fast) {
+        $self->run_1();
+    }
+}
